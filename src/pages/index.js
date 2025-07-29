771f2fb0750fc6c18f2744524975c72d50e1eee0
SideeID <sandipratama164026@gmail.com>
2025-07-29 21:20:37 +0700

    refactor: comment out TestimonialSection import and usage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Plus_Jakarta_Sans } from 'next/font/google'
 import HeroSection from '@/components/sections/Hero'
 import AboutSection from '@/components/sections/About'
 import ProductSection from '@/components/sections/Product'
-import TestimonialSection from '@/components/sections/Testimonial'
+// import TestimonialSection from '@/components/sections/Testimonial'
 import FoundersSection from '@/components/sections/Founder'
 import FAQSection from '@/components/sections/FAQ'
 import Footer from '@/components/layout/Footer'
@@ -26,7 +26,7 @@ export default function Home() {
         <div className='max-w-6xl mx-auto px-6 pb-8 flex flex-col gap-18'>
           <AboutSection />
           <ProductSection />
-          <TestimonialSection />
+          {/* <TestimonialSection /> */}
           <FoundersSection />
           <FAQSection />
         </div>
